508a2854d12b65defdfac670c5ccc86f9c7026c0
Shahriar262 <srh.2622@gmail.com>
2025-12-17 20:19:57 +0600

    added JWT middleware

--- index.js
+++ index.js
@@ -4,8 +4,31 @@ const cors = require("cors");
 const app = express()
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+// middleware
+app.use(
+  cors({
+    origin: [process.env.CLIENT_DOMAIN],
+    credentials: true,
+    optionSuccessStatus: 200,
+  })
+);
 app.use(express.json());
+
+
+// jwt middlewares
+const verifyJWT = async (req, res, next) => {
+  const token = req?.headers?.authorization?.split(" ")[1];
+  if (!token) return res.status(401).send({ message: "Unauthorized Access!" });
+  try {
+    const decoded = await admin.auth().verifyIdToken(token);
+    req.tokenEmail = decoded.email;
+    next();
+  } catch (err) {
+    console.log(err);
+    return res.status(401).send({ message: "Unauthorized Access!", err });
+  }
+};
+
 
 app.get('/', (req, res) => {
   res.send('Welcome to AssetVerse Server')
